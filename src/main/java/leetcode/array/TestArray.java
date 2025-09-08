3275ef7b04350b3f3b19a047ab5a5a325f8044de
longxuebin <longxuebin@mistong.com>
2025-09-08 13:28:55 +0800

    init

---
+++ src/main/java/leetcode/array/TestArray.java
@@ -0,0 +1,29 @@
+package leetcode.array;
+
+public class TestArray {
+    public static void main(String[] args) {
+
+        int[] prices=new int[]{12,9,8,7,6};
+        System.out.println(maxProfit(prices));
+    }
+
+    /**
+     * 股票最佳购买时机(一次遍历)
+     *
+     * @param prices
+     * @return
+     */
+    public static int maxProfit(int[] prices) {
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice >= maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
---
+++ src/test/java/cn/itcast/source/TestSourceServer.java
@@ -0,0 +1,31 @@
+package cn.itcast.source;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class TestSourceServer {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new LoggingHandler());
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                System.out.println("ctx=" + ctx + ",msg=" + msg);
+                            }
+                        });
+                    }
+                }).bind(8080);
+    }
+}
